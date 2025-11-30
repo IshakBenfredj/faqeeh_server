6bf1d84479fa5d65f74fadc5fc71ecbcfbe85b1a
Ishak Benfredj <107993434+IshakBenfredj@users.noreply.github.com>
2025-11-30 14:25:42 +0100

    Update Course.js

--- src/models/Course.js
+++ src/models/Course.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const courseSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String },
     price: { type: String, required: true },
     originalPrice: { type: String },
     image: { type: String, required: true },
